8aa7e8e09a719f93a994fbec8043411c60d2e114
Daniel <da.ortega@alumnos.upm.es>
2024-08-18 12:47:26 +0200

    Banned MicroHD from random items

---
+++ ItemRandomizerPlugin_SCPSL/RoomPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using UnityEngine;
+
+namespace JadeLib.Features.API.RoomPoint {
+    [Serializable]
+    public class RoomPointObject {
+        public RoomPointObject() {
+        }
+
+        public RoomPointObject(RoomType type, Vector3 relative) {
+            this.roomType = type;
+            this.relativePosition = relative;
+        }
+
+        public RoomPointObject(Vector3 mapPosition) : this(Room.Get(mapPosition).Type, mapPosition) {
+        }
+
+        public RoomType roomType = RoomType.Unknown;
+
+        public SerializedVector3 relativePosition = Vector3.zero;
+    }
+}
